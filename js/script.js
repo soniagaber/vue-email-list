07e3f3109fcc39d13dcb258d4fbfc4e2c102caca
soniagaber <soniagaber90@gmail.com>
2023-03-22 17:24:20 +0100

    bonus fatto

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        mail: []
+        mail: [],
+        finito:false
       }
     },
     
@@ -11,6 +12,7 @@ const { createApp } = Vue
         for(let i=0; i<10; i++){
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res) => {
             this.mail.push(res.data.response);
+            if(i==9)this.finito=true
         })
         }
     }
